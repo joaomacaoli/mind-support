54bef0d05af5d6fb3f530031acf37607d82cbeef
João Macaoli <devjoaom@gmail.com>
2025-02-12 11:25:20 -0300

    feat: adicionado base da topbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import Topbar from "./components/topbar/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Topbar />
       <App />
     </BrowserRouter>
   </React.StrictMode>
