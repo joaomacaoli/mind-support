04a53d1d2de4f4f65c8d9633274127e4c94a6c16
Vinicius <vinniciussramallho98@gmail.com>
2025-02-18 23:50:07 -0300

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Testimonials from "./pages/AnonymousTestimonialsArea";
 import SupportGroups from "./pages/support-groups";
+import Login from "./pages/LoginPage/login";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/anonymousTestimonial" element={<Testimonials />} />
+      <Route path="/anonymoustestimonials" element={<Testimonials />} />
       <Route path="/supportgroup" element={<SupportGroups />} />
+      <Route path="/login" element={<Login/>}/>
     </Routes>
-  );
+  );  
 }
 
 export default App;
