3a341b604c2fdd17f293b9e4497bde598b2d997c
João Macaoli <devjoaom@gmail.com>
2025-02-25 12:12:05 -0300

    feat: integração com o backend

---
+++ src/services/testimonials.js
@@ -0,0 +1,29 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log("API URL:", apiUrl);
+
+export async function getAllTestimonials() {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/testmonials`);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar depoimentos");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Erro ao buscar depoimentos:", error);
+    return [];
+  }
+}
+
+// export async function getAllUsers() {
+//   try {
+//     const response = await fetch(`${env}/users`, {
+//       next: {
+//         revalidate: 10,
+//       },
+//     });
+//     const users = await response.json();
+//     return users;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: "http://localhost:3000",
+    },
+  },
   plugins: [react()],
-})
+});
