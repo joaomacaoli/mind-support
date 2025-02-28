747bc8cae12a8de39b0663892886b8ee674d25dd
Vinicius <vinniciussramallho98@gmail.com>
2025-02-28 11:46:16 -0300

    adicionando rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import "./App.css";
+import "./index.css";
 import { Route, Routes } from "react-router-dom";
 import Testimonials from "./pages/AnonymousTestimonialsArea";
 import SupportGroups from "./pages/support-groups";
 import Login from "./pages/LoginPage/login";
-
+import Home from "./pages/HomePage";
+import { RegisterUsers } from "./pages/RegisterUsers";
 
 function App() {
   return (
     <Routes>
       <Route path="/testimonials" element={<Testimonials />} />
       <Route path="/supportgroup" element={<SupportGroups />} />
-      <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={< Login/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/register" element={<RegisterUsers/>}/>
     </Routes>
   );  
 }
