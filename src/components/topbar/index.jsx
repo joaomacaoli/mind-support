c650861633ef3aefa17afa8dcf19a302eb2b3737
Vinicius <vinniciussramallho98@gmail.com>
2025-02-28 11:41:27 -0300

    estilizando a topbar

--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -1,17 +1,37 @@
-import { NavLink } from "react-router-dom";
 import "./styles.css";
-
+import brainLogo from "./assets/cerebro2.png";
+import { Link } from "react-router-dom";
 export default function Topbar() {
   return (
-    <nav className="container-nav">
-      <div>
-        <NavLink to="/">LOGO</NavLink>
-      </div>
-      <div className="container-top-bar-menu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/supportgroup">Suporte</NavLink>
-        <NavLink to="/testimonials">Depoimentos</NavLink>
-      </div>
-    </nav>
+    <header className="header-home">
+      <nav className="navbar">
+        <div className="navbar-logo">
+          <Link to={"/"}>
+            <img src={brainLogo} alt="Brain Logo" className="img-logo-nav" />
+          </Link>
+        </div>
+
+        <ul className="nav-list">
+          <Link to={"/"} className="nav-link">
+            <li className="nav-item"> Home </li>
+          </Link>
+          <Link to={"testimonials"} className="nav-link">
+            <li className="nav-item"> Depoimentos </li>
+          </Link>
+          <Link to={"supportgroup"} className="nav-link">
+            <li className="nav-item"> Apoio </li>
+          </Link>
+        </ul>
+
+        <div className="btn-group">
+          <Link to={"/login"}>
+            <button className="btn-login"> Login </button>
+          </Link>
+          <Link to={"/register"}>
+            <button className="btn-sign-up"> Sing Up</button>
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
