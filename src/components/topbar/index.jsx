7abe4f1a62703925b3b389f94af5166d630dab8b
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-03-20 21:02:50 -0300

    Update index.jsx
    add NavLink para Login e SingUp, além da logo

--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -1,17 +1,25 @@
 import { NavLink } from "react-router-dom";
 import "./styles.css";
+import brainLogo from './assets/green-brain.png';
 
 export default function Topbar() {
   return (
+  
     <nav className="container-nav">
-      <div>
-        <NavLink to="/">LOGO</NavLink>
-      </div>
-      <div className="container-top-bar-menu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/supportgroup">Suporte</NavLink>
-        <NavLink to="/anonymousTestimonial">Depoimentos</NavLink>
-      </div>
+          <div className="container-logo"> 
+              <img src={brainLogo} alt=" Brain Logo"/> 
+          </div>
+          
+          <div className="container-top-bar-menu">
+              <NavLink to="/homepage">Home</NavLink>
+              <NavLink to="/supportgroup">Suporte</NavLink>
+              <NavLink to="/anonymousTestimonial">Depoimentos</NavLink>
+          </div>
+
+          <div className="btn-group-login">
+              <NavLink to="/supportgroup" className="button login">Login</NavLink>
+              <NavLink to="/generalRegistration" className="button sing-up">Sing Up</NavLink>
+          </div>
     </nav>
   );
 }
