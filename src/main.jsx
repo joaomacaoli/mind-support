ca405ff71798edbe9ba0ba5958e11fd073ed4fe4
Chrystina <144075668+chryssstina@users.noreply.github.com>
2025-03-20 21:00:39 -0300

    Update main.jsx
    add useLocation para esconder a TopBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
 import Topbar from "./components/topbar/index.jsx";
+
+
+const Main = () => {
+  const location = useLocation();
+  // onde não vai aparecer a TopBar
+  const showTopbar = location.pathname !== "/generalRegistration" &&
+                    location.pathname !== "/doctorRegistration" &&
+                    location.pathname !== "/patientRegistration" ; 
+
+  return (
+    <>
+      {showTopbar && <Topbar />}
+      <App />
+    </>
+  );
+};
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Topbar />
-      <App />
+      {/*<Topbar />
+      <App /> */}
+      <Main />
     </BrowserRouter>
   </React.StrictMode>
 );
