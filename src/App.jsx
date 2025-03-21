4dfcb3cab8da334077e762d7962b9f2211d46a2e
Vinicius <vinniciussramallho98@gmail.com>
2025-03-21 17:15:36 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 // import Testimonials from "./pages/AnonymousTestimonialsArea";
 // import SupportGroups from "./pages/support-groups";
-import Homepage from "./pages/Home/home";
+// import Homepage from "./pages/Home/home";
 import GeneralRegistration from "./pages/generalRegistration";
 import DoctorRegistration from "./pages/doctorRegistration";
 import PatientRegistration from "./pages/patientRegistration";
 
+import Login from "./app/login";
 import HomePage from "./app/home";
+import RegisterUsers from "./app/registers";
 import TestimonialsPage from "./app/testimonials";
 import SupportGroupsPage from "./app/support-groups";
+import Construction from "./app/building";
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/testimonials" element={<TestimonialsPage />} />
       <Route path="/support-groups" element={<SupportGroupsPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registers" element={<RegisterUsers />} />
+      <Route path="/construction" element={<Construction />} />
 
       <Route path="/generalRegistration" element={<GeneralRegistration />} />
       <Route path="/doctorRegistration" element={<DoctorRegistration />} />
