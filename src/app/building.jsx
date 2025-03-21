25d4323031197be5d499e37bfcc8f84ff2c3d59a
Vinicius <vinniciussramallho98@gmail.com>
2025-03-21 17:19:19 -0300

    create screen in building for tests

---
+++ src/app/building.jsx
@@ -0,0 +1,12 @@
+
+import UnderConstruction from "../components/UnderConstruction";
+
+function Construction (){
+  return (
+    <div>
+      <UnderConstruction />
+    </div>
+  );
+}
+
+export default Construction;
